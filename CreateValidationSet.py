bd15bbdd1c1f062d1f3e9b2c806ff1d4c7957eef
Matthew Brinard <matthewbrinard@gmail.com>
2020-06-08 03:53:00 -0400

    Update CreateValidationSet.py

--- CreateValidationSet.py
+++ CreateValidationSet.py
@@ -19,17 +19,20 @@ except Exception as e:
 if os.path.exists('./chest_xray/val'):
     print('The validation dataset already exists.')
 else:
+    # Take 10% of each class (rounded to the nearest batch) from the training dataset and create the validation dataset.
     os.mkdir('./chest_xray/val')
     os.mkdir('./chest_xray/val/NORMAL')
     os.mkdir('./chest_xray/val/PNEUMONIA')
 
     train_normal = os.listdir(train_NORMAL_dir)
-    for file in random.sample(train_normal, 80):
+    num_normal_batches = int((len(train_normal)*0.1)/16)
+    for file in random.sample(train_normal, (16 * num_normal_batches)):
         path_normal = os.path.join(train_NORMAL_dir, file)
         shutil.move(path_normal, './chest_xray/val/NORMAL')
 
     train_pneumonia = os.listdir(train_PNEUMONIA_dir)
-    for file in random.sample(train_pneumonia, 80):
+    num_pneumonia_batches = int((len(train_pneumonia) * 0.1) / 16)
+    for file in random.sample(train_pneumonia, (16 * num_pneumonia_batches)):
         path_pneumonia = os.path.join(train_PNEUMONIA_dir, file)
         shutil.move(path_pneumonia, './chest_xray/val/PNEUMONIA')
 
